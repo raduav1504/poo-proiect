6c08991bac9471c2fce2f8899e3351e667c8370f
raduav1504 <radu.avadanei.ichb@gmail.com>
2025-05-19 00:58:51 +0300

    Create Bike.h

---
+++ include/Bike.h
@@ -0,0 +1,20 @@
+#pragma once
+#include "Equipment.h"
+
+class Bike : public Equipment {
+public:
+    Bike(double resistance);
+    Bike(const Bike& other);
+    Bike& operator=(Bike other);
+    ~Bike() override = default;
+
+    std::unique_ptr<Equipment> clone() const override;
+
+protected:
+    void print(std::ostream& os) const override;
+    void do_use(int minutes) override;
+
+private:
+    double resistance_;
+    friend void swap(Bike& a, Bike& b);
+};
