79331e91e21fa286848a898fc75fc742df50d0b9
raduav1504 <radu.avadanei.ichb@gmail.com>
2025-05-19 19:10:39 +0300

    Update Treadmill.cpp

--- src/Treadmill.cpp
+++ src/Treadmill.cpp
@@ -1,41 +1,28 @@
 #include "Treadmill.h"
 #include "Exceptions.h"
-#include "Equipment.h"
 #include <algorithm>
 #include <iostream>
-#include <utility>
 
-Treadmill::Treadmill(double max_speed)
-    : Equipment("Treadmill"), max_speed_(max_speed) {
-    if (max_speed_ <= 0) {
+Treadmill::Treadmill(double maxSpeed)
+  : Equipment("Treadmill"), maxSpeed_(maxSpeed)
+{
+    if (maxSpeed <= 0)
         throw EquipmentError("max_speed must be positive");
-    }
 }
 
-Treadmill::Treadmill(const Treadmill& other)
-    : Equipment(other), max_speed_(other.max_speed_) {}
-
-Treadmill& Treadmill::operator=(Treadmill other) {
-    swap(*this, other);
-    return *this;
+void Treadmill::use(int minutes) {
+    if (minutes < 0) throw UsageError("negative minutes");
+    std::cout << name_ << ": running at up to "
+              << maxSpeed_ << " km/h for "
+              << minutes << " min\n";
 }
 
 std::unique_ptr<Equipment> Treadmill::clone() const {
     return std::make_unique<Treadmill>(*this);
 }
 
-void Treadmill::print(std::ostream& os) const {
-    Equipment::print(os);
-    os << " (max_speed=" << max_speed_ << ")";
-}
-
-void Treadmill::do_use(int minutes) {
-    std::cout << "Running on treadmill at up to " << max_speed_
-              << " km/h for " << minutes << " minutes.\n";
-}
-
-void swap(Treadmill& a, Treadmill& b) {
+void swap(Treadmill& a, Treadmill& b) noexcept {
     using std::swap;
-    swap(static_cast<Equipment&>(a), static_cast<Equipment&>(b));
-    swap(a.max_speed_, b.max_speed_);
+    swap(a.name_,      b.name_);
+    swap(a.maxSpeed_,  b.maxSpeed_);
 }
