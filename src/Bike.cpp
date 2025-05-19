86e2cdafe04297b9e02b38ec2e0d200c2fed28c4
raduav1504 <radu.avadanei.ichb@gmail.com>
2025-05-19 19:10:26 +0300

    Update Bike.cpp

--- src/Bike.cpp
+++ src/Bike.cpp
@@ -1,41 +1,28 @@
 #include "Bike.h"
 #include "Exceptions.h"
-#include "Equipment.h"
 #include <algorithm>
 #include <iostream>
-#include <utility>
 
 Bike::Bike(double resistance)
-    : Equipment("Bike"), resistance_(resistance) {
-    if (resistance_ < 0) {
+  : Equipment("Bike"), resistance_(resistance)
+{
+    if (resistance < 0)
         throw EquipmentError("resistance must be non-negative");
-    }
 }
 
-Bike::Bike(const Bike& other)
-    : Equipment(other), resistance_(other.resistance_) {}
-
-Bike& Bike::operator=(Bike other) {
-    swap(*this, other);
-    return *this;
+void Bike::use(int minutes) {
+    if (minutes < 0) throw UsageError("negative minutes");
+    std::cout << name_ << ": pedaling at resistance "
+              << resistance_ << " for "
+              << minutes << " min\n";
 }
 
 std::unique_ptr<Equipment> Bike::clone() const {
     return std::make_unique<Bike>(*this);
 }
 
-void Bike::print(std::ostream& os) const {
-    Equipment::print(os);
-    os << " (resistance=" << resistance_ << ")";
-}
-
-void Bike::do_use(int minutes) {
-    std::cout << "Cycling with resistance " << resistance_
-              << " for " << minutes << " minutes.\n";
-}
-
-void swap(Bike& a, Bike& b) {
+void swap(Bike& a, Bike& b) noexcept {
     using std::swap;
-    swap(static_cast<Equipment&>(a), static_cast<Equipment&>(b));
-    swap(a.resistance_, b.resistance_);
+    swap(a.name_,       b.name_);       // base protected member
+    swap(a.resistance_, b.resistance_); // derived
 }
