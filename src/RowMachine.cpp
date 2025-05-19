eba939b1835db7196af9c8eb873c9384066ece8f
raduav1504 <radu.avadanei.ichb@gmail.com>
2025-05-19 19:10:47 +0300

    Update RowMachine.cpp

--- src/RowMachine.cpp
+++ src/RowMachine.cpp
@@ -1,41 +1,28 @@
 #include "RowMachine.h"
 #include "Exceptions.h"
-#include "Equipment.h"
 #include <algorithm>
 #include <iostream>
-#include <utility>
 
-RowMachine::RowMachine(int stroke_rate)
-    : Equipment("RowMachine"), stroke_rate_(stroke_rate) {
-    if (stroke_rate_ <= 0) {
+RowMachine::RowMachine(int strokeRate)
+  : Equipment("RowMachine"), strokeRate_(strokeRate)
+{
+    if (strokeRate <= 0)
         throw EquipmentError("stroke_rate must be positive");
-    }
 }
 
-RowMachine::RowMachine(const RowMachine& other)
-    : Equipment(other), stroke_rate_(other.stroke_rate_) {}
-
-RowMachine& RowMachine::operator=(RowMachine other) {
-    swap(*this, other);
-    return *this;
+void RowMachine::use(int minutes) {
+    if (minutes < 0) throw UsageError("negative minutes");
+    std::cout << name_ << ": rowing at rate "
+              << strokeRate_ << " spm for "
+              << minutes << " min\n";
 }
 
 std::unique_ptr<Equipment> RowMachine::clone() const {
     return std::make_unique<RowMachine>(*this);
 }
 
-void RowMachine::print(std::ostream& os) const {
-    Equipment::print(os);
-    os << " (stroke_rate=" << stroke_rate_ << ")";
-}
-
-void RowMachine::do_use(int minutes) {
-    std::cout << "Rowing at " << stroke_rate_
-              << " strokes/min for " << minutes << " minutes.\n";
-}
-
-void swap(RowMachine& a, RowMachine& b) {
+void swap(RowMachine& a, RowMachine& b) noexcept {
     using std::swap;
-    swap(static_cast<Equipment&>(a), static_cast<Equipment&>(b));
-    swap(a.stroke_rate_, b.stroke_rate_);
+    swap(a.name_,        b.name_);
+    swap(a.strokeRate_,  b.strokeRate_);
 }
