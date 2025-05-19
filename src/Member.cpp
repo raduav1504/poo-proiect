eccd3d7211a1c8678e8a71a78fb1061f0982752f
raduav1504 <radu.avadanei.ichb@gmail.com>
2025-05-19 19:02:34 +0300

    Update Member.cpp

--- src/Member.cpp
+++ src/Member.cpp
@@ -1,34 +1,43 @@
 #include "Member.h"
 #include "Exceptions.h"
+#include "Bike.h"
+#include "Treadmill.h"
+#include "RowMachine.h"
+
 #include <iostream>
 #include <utility>
+#include <algorithm>  // for std::swap
 
 Member::Member(std::string name)
-    : name_(std::move(name)) {
+    : name_(std::move(name))
+{
     if (name_.empty()) {
         throw MemberError("empty name");
     }
 }
 
 Member::Member(const Member& other)
-    : name_(other.name_) {
+    : name_(other.name_)
+{
     for (const auto& eq : other.equipment_) {
         equipment_.push_back(eq->clone());
     }
 }
 
-Member& Member::operator=(Member other) {
+Member& Member::operator=(Member other)
+{
     swap(*this, other);
     return *this;
 }
 
-void Member::addEquipment(std::unique_ptr<Equipment> eq) {
+void Member::addEquipment(std::unique_ptr<Equipment> eq)
+{
     equipment_.push_back(std::move(eq));
 }
 
-void Member::useEquipment(const std::string& eqName, int minutes) {
-    for (auto& eq : equipment_) {
-        // downcast exemplu: căutăm bicicleta după nume
+void Member::useEquipment(const std::string& eqName, int minutes)
+{
+    for (const auto& eq : equipment_) {
         if (dynamic_cast<Bike*>(eq.get()) && eqName == "Bike") {
             eq->use(minutes);
             return;
@@ -45,7 +54,8 @@ void Member::useEquipment(const std::string& eqName, int minutes) {
     throw UsageError("no equipment named " + eqName);
 }
 
-std::ostream& operator<<(std::ostream& os, const Member& m) {
+std::ostream& operator<<(std::ostream& os, const Member& m)
+{
     os << "Member[" << m.name_ << "] owns:\n";
     for (const auto& eq : m.equipment_) {
         os << "  - " << *eq << "\n";
@@ -53,8 +63,9 @@ std::ostream& operator<<(std::ostream& os, const Member& m) {
     return os;
 }
 
-void swap(Member& a, Member& b) {
+void swap(Member& a, Member& b) noexcept
+{
     using std::swap;
-    swap(a.name_, b.name_);
-    swap(a.equipment_, b.equipment_);
+    swap(a.name_,       b.name_);
+    swap(a.equipment_,  b.equipment_);
 }
