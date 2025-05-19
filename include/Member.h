62eb55cad31c996041ed50a093a1036001ffa328
raduav1504 <radu.avadanei.ichb@gmail.com>
2025-05-19 19:09:49 +0300

    Update Member.h

--- include/Member.h
+++ include/Member.h
@@ -1,23 +1,27 @@
-#pragma once
+#ifndef MEMBER_H
+#define MEMBER_H
+
+#include "Equipment.h"
+#include <memory>
+#include <ostream>
 #include <string>
 #include <vector>
-#include <memory>
-#include "Equipment.h"
 
 class Member {
 public:
     explicit Member(std::string name);
-    Member(const Member& other);
-    Member& operator=(Member other);
-    ~Member() = default;
+    Member(const Member&);
+    Member& operator=(Member);
 
     void addEquipment(std::unique_ptr<Equipment> eq);
     void useEquipment(const std::string& eqName, int minutes);
 
     friend std::ostream& operator<<(std::ostream& os, const Member& m);
+    friend void swap(Member& a, Member& b) noexcept;
 
 private:
     std::string name_;
     std::vector<std::unique_ptr<Equipment>> equipment_;
-    friend void swap(Member& a, Member& b);
 };
+
+#endif // MEMBER_H
