f06309c60f4a75acbb8b594b025240218e9ae694
raduav1504 <radu.avadanei.ichb@gmail.com>
2025-05-19 19:09:23 +0300

    Update Bike.h

--- include/Bike.h
+++ include/Bike.h
@@ -1,20 +1,26 @@
-#pragma once
+#ifndef BIKE_H
+#define BIKE_H
+
 #include "Equipment.h"
 
 class Bike : public Equipment {
 public:
-     explicit Bike(double resistance);
-    Bike(const Bike& other);
-    Bike& operator=(Bike other);
+    explicit Bike(double resistance);
+    Bike(const Bike&) = default;
+    Bike& operator=(const Bike&) = default;
     ~Bike() override = default;
 
+    void use(int minutes) override;
     std::unique_ptr<Equipment> clone() const override;
 
-protected:
-    void print(std::ostream& os) const override;
-    void do_use(int minutes) override;
+    friend void swap(Bike& a, Bike& b) noexcept;
 
 private:
     double resistance_;
-    friend void swap(Bike& a, Bike& b);
+
+    void printDetails(std::ostream& os) const override {
+        os << " (resistance=" << resistance_ << ")";
+    }
 };
+
+#endif // BIKE_H
