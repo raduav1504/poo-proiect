337eb2764e70ac0382e3ab0c7f713bcf35345d8e
raduav1504 <radu.avadanei.ichb@gmail.com>
2025-05-19 19:10:18 +0300

    Update Equipment.cpp

--- src/Equipment.cpp
+++ src/Equipment.cpp
@@ -1,16 +1,10 @@
 #include "Equipment.h"
 #include "Exceptions.h"
+#include <utility>
 
 Equipment::Equipment(std::string name)
-    : name_(std::move(name)) {
-    if (name_.empty()) {
-        throw EquipmentError("empty name");
-    }
-}
-
-void Equipment::use(int minutes) {
-    if (minutes <= 0) {
-        throw EquipmentError("usage time must be positive");
-    }
-    do_use(minutes);
+  : name_(std::move(name))
+{
+    if (name_.empty())
+        throw EquipmentError("empty equipment name");
 }
