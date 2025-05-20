fae10eb7c2cb5fc083d8fa40cc88c15ea50d154b
raduav1504 <radu.avadanei.ichb@gmail.com>
2025-05-20 05:38:03 +0300

    Update treadmill.hpp

--- include/treadmill.hpp
+++ include/treadmill.hpp
@@ -7,7 +7,7 @@ public:
     std::shared_ptr<EquipmentBase> clone() const override;
 
     /// Domain-specific API: adjust the running speed
-    void setMaxSpeed(double s) noexcept { maxSpeed_ = s; }
+    vvoid setMaxSpeed(double s) { maxSpeed_ = s; }
 
 protected:
     Treadmill(const Treadmill& other);
