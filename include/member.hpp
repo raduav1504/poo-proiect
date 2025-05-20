7cd4812835d4c06140c2c6de6b7f83999ba5706f
raduav1504 <radu.avadanei.ichb@gmail.com>
2025-05-20 04:21:03 +0300

    Update and rename Member.h to member.hpp

---
+++ include/member.hpp
@@ -0,0 +1,22 @@
+#pragma once
+#include <string>
+#include <vector>
+#include <memory>
+#include <iostream>
+#include "equipment.hpp"
+
+class Member {
+public:
+    explicit Member(std::string name);
+    Member(const Member& other);
+    Member& operator=(Member other);
+    friend void swap(Member& a, Member& b);
+
+    void addEquipment(const std::shared_ptr<EquipmentBase>& eq);
+    void doWorkout(const std::string& eqId, int minutes);
+    friend std::ostream& operator<<(std::ostream& os, const Member& m);
+
+private:
+    std::string name_;
+    std::vector<std::shared_ptr<EquipmentBase>> equipment_;
+};
