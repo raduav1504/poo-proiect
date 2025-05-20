9ce08ae48c553e04da80fc3e90674957bb0e06f7
raduav1504 <radu.avadanei.ichb@gmail.com>
2025-05-20 04:19:09 +0300

    Update and rename EquipmentBase.cpp to equipment.cpp

---
+++ src/equipment.cpp
@@ -0,0 +1,29 @@
+#include "equipment.hpp"
+
+int EquipmentBase::count_ = 0;
+
+EquipmentBase::EquipmentBase(const std::string& id)
+  : id_(id) { ++count_; }
+
+EquipmentBase::~EquipmentBase() { --count_; }
+
+void EquipmentBase::operate(int minutes) {
+    if (minutes <= 0) throw InvalidDurationException(minutes);
+    doOperate(minutes);
+}
+
+const std::string& EquipmentBase::id() const noexcept {
+    return id_;
+}
+
+int EquipmentBase::getCount() noexcept {
+    return count_;
+}
+
+EquipmentBase::EquipmentBase(const EquipmentBase& other)
+  : id_(other.id_) { ++count_; }
+
+EquipmentBase& EquipmentBase::operator=(const EquipmentBase& other) {
+    id_ = other.id_;
+    return *this;
+}
