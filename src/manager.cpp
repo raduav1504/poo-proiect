fae10746f340089123b3bffc27363e0f3237ebf9
raduav1504 <radu.avadanei.ichb@gmail.com>
2025-05-20 04:21:55 +0300

    Create manager.cpp

---
+++ src/manager.cpp
@@ -0,0 +1,24 @@
+#include "manager.hpp"
+#include "exceptions.hpp"
+
+void ClubManager::addMember(const std::string& name) {
+    members_.emplace(name, std::make_shared<Member>(name));
+}
+
+void ClubManager::addEquipmentToMember(const std::string& name,
+                                       const std::shared_ptr<EquipmentBase>& eq)
+{
+    auto it = members_.find(name);
+    if (it == members_.end())
+        throw MemberNotFoundException(name);
+    it->second->addEquipment(eq);
+}
+
+void ClubManager::workout(const std::string& name,
+                          const std::string& eqId, int minutes)
+{
+    auto it = members_.find(name);
+    if (it == members_.end())
+        throw MemberNotFoundException(name);
+    it->second->doWorkout(eqId, minutes);
+}
