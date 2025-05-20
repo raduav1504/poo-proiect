afd80f36499fb7447861db67efc96431caaf104b
raduav1504 <radu.avadanei.ichb@gmail.com>
2025-05-20 04:21:17 +0300

    Update and rename Member.cpp to member.cpp

---
+++ src/member.cpp
@@ -0,0 +1,42 @@
+#include "member.hpp"
+#include "exceptions.hpp"
+#include <algorithm>
+
+Member::Member(std::string name)
+  : name_(std::move(name)) {}
+
+Member::Member(const Member& other)
+  : name_(other.name_) {
+    for (auto& eq : other.equipment_)
+        equipment_.push_back(eq->clone());
+}
+
+Member& Member::operator=(Member other) {
+    swap(*this, other);
+    return *this;
+}
+
+void swap(Member& a, Member& b) {
+    using std::swap;
+    swap(a.name_, b.name_);
+    swap(a.equipment_, b.equipment_);
+}
+
+void Member::addEquipment(const std::shared_ptr<EquipmentBase>& eq) {
+    equipment_.push_back(eq);
+}
+
+void Member::doWorkout(const std::string& eqId, int minutes) {
+    auto it = std::find_if(equipment_.begin(), equipment_.end(),
+        [&](auto& e){ return e->id() == eqId; });
+    if (it == equipment_.end())
+        throw EquipmentNotFoundException(eqId);
+    (*it)->operate(minutes);
+}
+
+std::ostream& operator<<(std::ostream& os, const Member& m) {
+    os << "Membru: " << m.name_ << "\n  Echipamente:\n";
+    for (auto& eq : m.equipment_)
+        os << "    - " << eq->id() << "\n";
+    return os;
+}
