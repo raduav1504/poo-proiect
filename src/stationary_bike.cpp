c72c32d41af3941252d9df690260e43b96182579
raduav1504 <radu.avadanei.ichb@gmail.com>
2025-05-20 04:34:49 +0300

    Update stationary_bike.cpp

--- src/stationary_bike.cpp
+++ src/stationary_bike.cpp
@@ -2,14 +2,17 @@
 #include <iostream>
 
 StationaryBike::StationaryBike(const std::string& id, double resistance)
-  : EquipmentBase(id), resistance_(resistance) {}
+  : EquipmentBase(id), resistance_(resistance)
+{}
 
 std::shared_ptr<EquipmentBase> StationaryBike::clone() const {
-    return std::make_shared<StationaryBike>(*this);
+    return std::shared_ptr<EquipmentBase>(new StationaryBike(*this));
 }
 
 StationaryBike::StationaryBike(const StationaryBike& other)
-  : EquipmentBase(other), resistance_(other.resistance_) {}
+  : EquipmentBase(other)
+  , resistance_(other.resistance_)
+{}
 
 void StationaryBike::doOperate(int min) {
     std::cout << "Bike " << id() << ": pedalat "
